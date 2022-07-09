1e700a2aa580effcfc5e1e823fe90f3397bc5649
João Victor Mundel <joaovitor20002@gmail.com>
2022-07-09 15:34:02 -0300

    Ajustes finalizados no backend

--- src/main/java/me/joao/bookstore/domain/Livro.java
+++ src/main/java/me/joao/bookstore/domain/Livro.java
@@ -8,7 +8,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
 
+import org.hibernate.validator.constraints.Length;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -25,8 +27,14 @@ public class Livro implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	@NotEmpty(message = "Campo TITULO é requerido.")
+	@Length(min = 3, max = 50, message = "O campo TITULO deve ter entre 3 e 50 caracteres.")
 	private String titulo;
+	@NotEmpty(message = "Campo AUTOR é requerido.")
+	@Length(min = 3, max = 50, message = "O campo AUTOR deve ter entre 3 e 50 caracteres.")
 	private String autor;
+	@NotEmpty(message = "Campo TEXTO é requerido.")
+	@Length(min = 10, message = "O campo TEXTO deve ter no mínimo 10 caracteres.")
 	private String texto;
 
 	@JsonIgnore
--- src/main/java/me/joao/bookstore/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/me/joao/bookstore/resources/exceptions/ResourceExceptionHandler.java
@@ -4,6 +4,8 @@ import javax.servlet.ServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -24,6 +26,21 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request) {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
 				e.getMessage());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
+	
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request) {
+		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Erro de validação dos campos.");
+		
+		for(FieldError f : e.getBindingResult().getFieldErrors()) {{
+			error.addErrors(f.getField(), f.getDefaultMessage());
+		}
+			
+		}
+		
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 
